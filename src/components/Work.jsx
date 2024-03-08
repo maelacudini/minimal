2b6d81783f8ddb5c6abc11eff25387454106ca91
maelacudini <maelacudini@gmail.com>
2024-03-08 10:37:30 +0100

    modifiche generali

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -59,22 +59,22 @@ const images = [
 const Work = () => {
   return (
     <div id="work">
-      <h1 className={style.workTitle}>
-        Simplifying web excellence: your brand experience.
-      </h1>
+      <div className={style.workTitle}>
+        <h1>Simplifying web excellence: your brand experience.</h1>
+      </div>
 
       <article id="projects">
         <h3 className={style.sectionTitle}>Our Projects</h3>
         <div className={style.gallery}>
           {images.map((image, index) => (
             <div key={index} className={style.card}>
-              <motion.a
+              <a
                 data-bs-toggle="modal"
                 data-bs-target={`#modal-${index}`}
-                whileHover={{ scale: 1.1 }}
                 className={`nav-link ${style.cardlink}`}
               >
-                <img
+                <motion.img
+                  whileHover={{ scale: 1.1 }}
                   src={image.url}
                   alt={image.title}
                   className="img-fluid"
@@ -84,7 +84,7 @@ const Work = () => {
                   <h3>{image.title}</h3>
                   <p>{image.description.slice(0, 30)}...</p>
                 </div>
-              </motion.a>
+              </a>
               <WorkProjects image={image} index={index} />
             </div>
           ))}
